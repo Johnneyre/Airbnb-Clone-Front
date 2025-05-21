779a87800ef404354d95e25017508a4df77191c7
Johnneyre <johnneyrez@gmail.com>
2025-05-21 08:55:44 -0400

    feat: add layout to main page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
-import { FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import {
+  FaIconLibrary,
+  FontAwesomeModule,
+} from '@fortawesome/angular-fontawesome';
 import { fontAwesomeIcons } from './shared/font-awesome-icons';
+import { NavbarComponent } from './layout/navbar/navbar.component';
+import { FooterComponent } from './layout/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ButtonModule, FontAwesomeModule],
+  imports: [
+    RouterOutlet,
+    ButtonModule,
+    FontAwesomeModule,
+    NavbarComponent,
+    FooterComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
@@ -19,6 +30,6 @@ export class AppComponent implements OnInit {
   }
 
   private initFontAwesome(): void {
-    this.faIconLibrary.addIcons(...fontAwesomeIcons)
+    this.faIconLibrary.addIcons(...fontAwesomeIcons);
   }
 }
